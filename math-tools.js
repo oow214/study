1071500e11c40f00dda578a833fe397dcc33ffd9
yeonwoo <oow214@gmail.com>
2021-08-09 10:12:47 +0900

    Module 연습

---
+++ math-tools.js
@@ -0,0 +1,25 @@
+/*
+function add(a, b) {
+    return a + b;
+}
+*/
+
+
+// exports.PI = 3.14;
+// exports.add = function add(a, b) { return a + b; };
+// exports.subtract = function subtract(a, b) { return a - b; };
+// exports.multiply = function multiply(a, b) { return a * b; };
+// exports.divide = function divide(a, b) { return a / b; };
+
+
+let calculator = {
+    PI: 3.14,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+
+module.exports = calculator;
+
+
