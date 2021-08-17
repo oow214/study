de8116946944ebdf993d8744ebeb0276a080dc72
oow214 <oow214@gmail.com>
2021-08-18 01:04:46 +0900

    자료형-typeof

--- dataType.js
+++ dataType.js
@@ -1,3 +1,4 @@
+/*
 //숫자형
 console.log((2 + 3) * 2);
 
@@ -16,5 +17,22 @@ console.log(7 !== 7 || 4 < 3); //false
 
 let x = 3;
 console.log(x > 4 || !(x > 2)); //false
+*/
+
+//자료형 확인 - typeof
+console.log(typeof 1);
+console.log(typeof '1');
+console.log(typeof `1`);
+console.log(typeof true);
+
+console.log(typeof "Hello" + "Yeonwoo!"); //StringYeonwoo가 출력됨.
+//typeof는 사칙연산자보다 우선순위가 높아서 먼저 연산됐다.
+console.log(typeof (8 - 3)); //괄호를 통해 우선순위 정해주기
+
+typeof (6 * 2 === 11 || 13 - 7 < 7 && !true);
+//typeof false; // boolean
+
+
+
 
 
