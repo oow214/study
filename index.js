4e1cb3f6028e0392ec554720b5b7d004ed1ba2a4
oow214 <oow214@gmail.com>
2021-08-18 00:43:34 +0900

    함수 파라미터 연습2

--- index.js
+++ index.js
@@ -2,21 +2,41 @@
 function hello() {
     console.log("Hello! Yeonwoo!");
     console.log("Wow!");
-}
+};
 
 hello();
 */
 
+/*
 function welcome(name) {
     console.log("안녕하세요 " + name + "님!");
-}
+};
 
 welcome("Yeonwoo");
 welcome("Dabin");
 
 function printSquare(x) {
     console.log(x * x); //거듭제곱 출력
-}
+};
 
 printSquare(2);
 printSquare(6);
+*/
+
+function printSum(num1, num2) {
+    console.log(num1 + num2);
+};
+
+printSum(10, 5);
+printSum(123, 4);
+
+function introduce(name, birth, nationality, job) {
+    console.log("안녕하세요!");
+    console.log("제 이름은 " + name + "입니다.");
+    console.log("생년월일은 " + birth + "이고요,");
+    console.log("국적은 " + nationality + "입니다.");
+    console.log("직업은 " + job + "입니다.");
+    console.log(">_<!");
+};
+
+introduce("김연우", "20020214", "대한민국", "대학생");
