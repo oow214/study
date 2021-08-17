ec7d347ec4a8b8524763f5b800491f289e8a8223
oow214 <oow214@gmail.com>
2021-08-18 01:12:22 +0900

    형변환

--- dataType.js
+++ dataType.js
@@ -19,6 +19,9 @@ let x = 3;
 console.log(x > 4 || !(x > 2)); //false
 */
 
+const { type } = require("os");
+
+/*
 //자료형 확인 - typeof
 console.log(typeof 1);
 console.log(typeof '1');
@@ -31,8 +34,31 @@ console.log(typeof (8 - 3)); //괄호를 통해 우선순위 정해주기
 
 typeof (6 * 2 === 11 || 13 - 7 < 7 && !true);
 //typeof false; // boolean
+*/
 
+//형 변환(Type Conversion)
+console.log(Number('10') + Number('5'));
+console.log(String(5) + String(10));
 
+let x = '문자';
+//let x = '123';
+console.log(x); //'문자'면 NaN, 숫자면 형변환된다.
+console.log(Number(x));
+console.log(typeof x);
+console.log(typeof Number(x));
 
+let y = false;
+console.log(y);
+console.log(Boolean(y)); // ''(빈문자열), 0, NaN은 false로 변한다.
+console.log(typeof y);
+console.log(typeof Boolean(y)); 
+//불린 값으로 형변했을 때 false가 나오는 값을 falsy값이라 한다.
 
-
+//NaN(not a number)
+//NaN은 숫자가 아님을 표시하는 값이지만 Number의 속성이기 때문에 Number의 자료형(객체)이라 인식이 된다.
+let y = NaN;
+let z = Number.NaN; //Number의 한 속성이라고 자세히 표시한 것 (객체.속성)
+let x = Number.MIN_VALUE; //Number 객체의 다른 속성 (0에 가까운 가장 작은 양수)
+console.log(typeof y); //number
+console.log(typeof z); //number
+console.log(typeof x); //number
