460d4691685ddc7984610690d3af1e6f2608195f
oow214 <oow214@gmail.com>
2021-08-18 00:38:27 +0900

    함수 파라미터 연습

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+/*
 function hello() {
     console.log("Hello! Yeonwoo!");
     console.log("Wow!");
 }
 
 hello();
-hello();
+*/
+
+function welcome(name) {
+    console.log("안녕하세요 " + name + "님!");
+}
+
+welcome("Yeonwoo");
+welcome("Dabin");
+
+function printSquare(x) {
+    console.log(x * x); //거듭제곱 출력
+}
+
+printSquare(2);
+printSquare(6);
