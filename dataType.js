f606ae8b92de0a5ec1fadccc4ab7efdbbd48bba2
oow214 <oow214@gmail.com>
2021-08-18 01:00:27 +0900

    자료형

---
+++ dataType.js
@@ -0,0 +1,20 @@
+//숫자형
+console.log((2 + 3) * 2);
+
+//몫만 구하기
+const a = Math.trunc(4 / 3);
+console.log(typeof a, a);
+
+//문자열
+console.log("연우"); //따옴표는 출력되지 않는다.
+console.log('안녕 나는 \'연우\'야! \'떡볶이\'는 진짜 맛있어!');
+console.log("푸른"+"하늘");
+
+//불 대수는 명제에 있어서 참 거짓을 나눈다!
+console.log(2 < 1 && 'yeonwoo' !== 'yeonwoo');
+console.log(7 !== 7 || 4 < 3); //false
+
+let x = 3;
+console.log(x > 4 || !(x > 2)); //false
+
+
