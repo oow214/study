aa8d20008c8d5d58a9348fdef05629f2d32fe9c0
oow214 <oow214@gmail.com>
2021-08-20 02:42:33 +0900

    객체에서 데이터 접근

--- object.js
+++ object.js
@@ -1,13 +1,23 @@
 //객체(object)
-console.log(typeof {
+let yeonwoo =  {
     name: "Yeonwoo",
     birth: 20020214,
     job: null,
-    love: {
+    'my love': {
         cake: "초코케이크",
         cookie: "버터쿠키"
     }
-})
+}
+
+console.log(typeof yeonwoo);
 
 
+//점 표기법 (objectName.propertyName)
+console.log(yeonwoo.birth);
 
+//대괄호 표기법 (objectName['propertyName'])
+console.log(yeonwoo['my love']);
+console.log(yeonwoo['my' + ' love']);
+
+let love = 'my love';
+console.log(yeonwoo[love]);
