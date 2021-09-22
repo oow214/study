aeef04d0721a6416bef7fe30afe569b5737f8d29
yeonwoo <oow214@gmail.com>
2021-09-22 19:17:35 +0900

    Sequelize를 이용한 데이터베이스 연습

--- cowork/app.js
+++ cowork/app.js
@@ -4,59 +4,75 @@ const express = require('express');
 const app = express();
 
 const db = require('./models/index');
+const member = require('./models/member');
 
 const { Member } = db;
 
 app.use(express.json()); 
 
-app.get('/api/members', (req, res) => {
+app.get('/api/members', async (req, res) => {
   const { team } = req.query;
 	if (team) {
-    const teamMembers = members.filter((m) => m.team === team);
+    // const teamMembers = await Member.findAll({ where: { team: team }});
+    const teamMembers = await Member.findAll({ where: { team }});
     res.send(teamMembers);
   } else {
+    const members = await Member.findAll();
     res.send(members);
   }
 });
 
-app.get('/api/members/:id', (req, res) => {
+app.get('/api/members/:id', async (req, res) => {
 	// const id = req.params.id;
   const { id } = req.params;  // params 객체에서 id라는 프로퍼티의 값만 id라는 변수에 담는 것이다.
-  const member = members.find((m) => m.id === Number(id));
+  const member = await Member.findOne({ where: { id } });
   if (member) {
     res.send(member);
   } else {
-    // res.status(404).send('There is no such member');
     res.status(404).send({ message: 'There is no member with the id!' });
   }
 });
 
-app.post('/api/members', (req, res) => {
+app.post('/api/members', async (req, res) => {
   const newMember = req.body;
-  members.push(newMember); // 배열에 추가
-  res.send(newMember); // 추가한 객체 리스폰스에 담아서 보냄
+  // const member = Member.build(newMember);
+  // await member.save(); // 배열에 추가
+  const member = await Member.create(newMember);
+  res.send(member); // 추가한 객체 리스폰스에 담아서 보냄
 });
 
-app.put('/api/members/:id', (req, res) => {
+// app.put('/api/members/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const newInfo = req.body; // 새 정보 body에 담기
+//   const result = await Member.update(newInfo, { where: { id } }) // id 찾기
+//   if (result[0]) {
+//     res.send({ message: `${result[0]} row(s) affected` }); // 바뀐 정보 담아서 보내기
+//   } else {
+//     res.status(404).send({ message: 'There is no member with the id!' });
+//   }
+// });
+
+app.put('/api/members/:id', async (req, res) => {
   const { id } = req.params;
-  const newInfo = req.body; // 새 정보 body에 담기
-  const member = members.find((m) => m.id === Number(id)); // id 찾기
-  if (member) {
-    Object.keys(newInfo).forEach((prop) => { // id 있으면 모든 프로퍼티 수정
+  const newInfo = req.body;
+  const member = await Member.findOne({ where: { id }});  // 해당 id값을 가진 row를 가져옴
+  if (result[0]) {
+    Object.keys(newInfo).forEach((prop) => {
       member[prop] = newInfo[prop];
     });
-    res.send(member); // 바뀐 정보 담아서 보내기
+    await member.save();
+    res.send(member);
   } else {
     res.status(404).send({ message: 'There is no member with the id!' });
   }
 });
 
-app.delete('/api/members/:id', (req, res) => {
+
+app.delete('/api/members/:id', async (req, res) => {
   const { id } = req.params;
-  const membersCount = members.length;
-  members = members.filter((member) => member.id !== Number(id));
-  if (members.length < membersCount) {
-    res.send({ message: 'Deleted' });
+  const deletedCount = await Member.destroy({ where: { id } });
+  if (deletedCount) {
+    res.send({ message: `${deletedCount} row(s) delected` });
   } else {
     res.status(404).send({ message: 'There is no member with the id!' });
   }
