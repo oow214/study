cfae0a07d19e770172933737f2b4b95a95bcd201
oow214 <oow214@gmail.com>
2021-09-26 03:22:26 +0900

    0906_class

--- Hello_Typescript/src/index.ts
+++ Hello_Typescript/src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age:number, gender?:string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "yeonwoo",
-  gender: "male",
-  age: 57
-}
+const yeonwoo = new Human("Yeonwoo", 57, "female");
 
 const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+  return `Hello ${person.name}, you are ${person}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(yeonwoo));
 
 export {};
