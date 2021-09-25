d4190feabc2cc175a3f425e55bc984f316691cee
yeonwoo <oow214@gmail.com>
2021-09-25 18:43:38 +0900

    0925_Props

--- yeonwoozzang/dicegame/src/index.js
+++ yeonwoozzang/dicegame/src/index.js
@@ -1,18 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const product = 'Macbook';
-const imageUrl = 'https://upload';
-
-function handleClick() {
-  alert('곧 도착합니다!');
-}
-
-ReactDOM.render(
-  <>
-    <h1>나만의 {product.toUpperCase()} 주문하기</h1>
-    <img src={imageUrl} alt="제품 사진" />
-    <button onClick={handleClick}>확인</button>
-  </>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
