c8a388a82f4c700166ff66dcd7590c7bac3a0a1d
oow214 <oow214@gmail.com>
2021-09-26 03:10:34 +0900

    0906_interface

--- Hello_Typescript/src/index.ts
+++ Hello_Typescript/src/index.ts
@@ -1,11 +1,19 @@
-const name = "yeonwoo",
-  age = 52,
-  gender = "male";
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-const sayHi = (name:string, age:number, gender?:string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const person = {
+  name: "yeonwoo",
+  gender: "male",
+  age: 57
+}
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-sayHi(name, age);
+console.log(sayHi(person));
 
 export {};
