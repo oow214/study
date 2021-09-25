2cb997f599ff12654164df01b63b11ea370a96d7
yeonwoo <oow214@gmail.com>
2021-09-25 19:58:24 +0900

    0925_컴포넌트 - 코드 정리

--- yeonwoozzang/dicegame/src/Board.js
+++ yeonwoozzang/dicegame/src/Board.js
@@ -1,14 +1,16 @@
 import Dice from './Dice';
 
-function Board({ name, color, num, sum, gameHistory }) {
+function Board({ name, color, gameHistory }) {
+  const num = gameHistory[gameHistory.length - 1] || 1;
+  const sum = gameHistory.reduce((a, b) => a + b, 0);
   return (
     <div>
-      <h2>{name}</h2>
+      <h1>{name}</h1>
       <Dice color={color} num={num} />
       <h2>총점</h2>
       <p>{sum}</p>
       <h2>기록</h2>
-      {gameHistory.join(', ')}
+      <p>{gameHistory.join(', ')}</p>
     </div>
   );
 }
