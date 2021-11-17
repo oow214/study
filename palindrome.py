e92582d9b562ad01fdcac29322e941c3297fbed3
yeonwoo <oow214@gmail.com>
2021-11-18 01:09:44 +0900

    211118 - 데이터사이언스_palindrome

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+"""
+def is_palindrome(word):
+    temp_word = word
+    word = list(word)
+    word.reverse()
+    answer = ''.join(word)
+    if answer == temp_word:
+        return True
+    return False
+"""
+
+def is_palindrome(word):
+    for left in range(len(word) // 2):
+        right = len(word) - left -1
+        if word[left] != word[right]:
+            return False
+    return True
+
+# 테스트
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
